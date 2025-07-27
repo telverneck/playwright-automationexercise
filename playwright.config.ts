a2251240e9235f7043578a0dbff664dc019a0fbe
telverneck <telverneck@hotmail.com>
2025-07-27 15:24:56 +0100

    feat: allure report integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }]], // Gera relatório HTML
+  reporter: [['html', { open: 'never' }], ['list'],
+  ['allure-playwright']], // Gera relatório HTML
   use: {
     headless: true,
     baseURL: process.env.BASE_URL,
