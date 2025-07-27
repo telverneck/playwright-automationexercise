b92a48a8f10fddec753ba97f186bf00de52dd386
telverneck <telverneck@hotmail.com>
2025-07-27 22:03:04 +0100

    ci: fix email Setup

--- tests/utils/emailReporter.js
+++ tests/utils/emailReporter.js
@@ -1,33 +1,34 @@
-const nodemailer = require('nodemailer');
-const path = require('path');
+const nodemailer = require("nodemailer");
+const path = require("path");
 
 async function sendEmailReport() {
     const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_SENDER,
         pass: process.env.EMAIL_PASSWORD,
-    },
+        },
     });
 
     const mailOptions = {
         from: process.env.EMAIL_SENDER,
         to: process.env.EMAIL_RECEIVER,
-        subject: '📊 Playwright Test Report',
-        text: 'Attached is the latest test report from Playwright.',
+        subject: "✅ Playwright Test Report",
+        text: "Attached is the latest Allure report.",
         attachments: [
-            {
-            filename: 'allure-report.zip',
-            path: path.resolve('./allure-report.zip'),
-            },
+        {
+            filename: "allure-report.zip",
+            path: path.resolve("./allure-report.zip"),
+        },
         ],
-    };    
+    };
 
     try {
         await transporter.sendMail(mailOptions);
-        console.log('✅ Email sent!');
-    }  catch (error) {
-        console.error('❌ Failed to send email:', error);
+        console.log("📬 Email sent with report!");
+    } catch (error) {
+        console.error("❌ Failed to send email:", error);
+        process.exit(1);
     }
 }
 
